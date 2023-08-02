9860a514b3bfc2304da493c7ee77902abe957f34
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-08-02 13:49:01 +0530

    small fix

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import TopBar from '@/components/topbar'
 export default function Home() {
 
   const [message, setMessage] = useState('');
-  const [user, setUser] = useState('Someone');
   const [messages, setMessages] = useState([]);
   const msgerChat = useRef(null);
 
