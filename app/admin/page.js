4aacdfee19ad26a188b21b060c4996e338605e0a
Sahithya Kattamuri <aythihas2003@gmail.com>
2023-09-26 02:22:16 +0530

    <add,fix>:[start and stop of model  button,admin components ]

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,6 +4,14 @@ import { useEffect, useState } from 'react'
 import { db } from '@/app/firebase'
 import { arrayUnion, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
 import TopBar from '@/components/topbar/topbar'
+import axios from "axios";
+import {
+  AiOutlineCloudUpload,
+  AiOutlineFileAdd,
+  AiOutlineShoppingCart,
+  AiOutlineCheckCircle,
+} from "react-icons/ai";
+
 
 export const getSuggestions = async () => {
   try {
@@ -99,89 +107,190 @@ const Admin = () => {
     setAnswer(e.target.value);
   }
 
+   const [selectedFile, setSelectedFile] = useState(null);
+   const [fileName, setFileName] = useState("Not selected file");
+   const [fileSelected, setFileSelected] = useState(false);
+
+   const handleFileChange = (event) => {
+     const file = event.target.files[0];
+     setSelectedFile(file);
+     setFileName(file.name);
+     setFileSelected(true);
+   };
+
+   const handleUpload = async () => {
+     const formData = new FormData();
+     formData.append("file", selectedFile);
+
+     try {
+       const response = await axios.post("/api/upload", formData, {
+         headers: {
+           "Content-Type": "multipart/form-data",
+         },
+       });
+       console.log(response.data);
+     } catch (error) {
+       console.error("Error:", error);
+     }
+   };
   return (
-    <div className="w-screen p-6 h-screen">
-      <TopBar admin={true}/>
-      <div className="dark:bg-black dark:text-white flex flex-col py-2 px-4">
-        <h1 className="text-xl text-center dark:text-gray-100">Admin Page</h1>
-        { authorised ? (
-          <div className="p-5 bg-white dark:bg-black transition-all ease-in-out rounded shadow-sm w-full">
-            <div>
-              <div className="flex min-h-[300px] items-center justify-center w-full">
-                <div className="rounded-xl overflow-hidden relative text-center p-4 group items-center flex flex-col max-w-sm hover:shadow-2xl transition-all duration-500 shadow-xl">
-                  <div className="text-gray-500 group-hover:scale-105 transition-all">
-                    <svg className="w-16 h-16" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" stroke-linejoin="round" stroke-linecap="round"></path>
-                    </svg>
+    <div className="h-full ">
+      <TopBar admin={true} />
+      <div className="dark:bg-black dark:text-white ">
+        <div className="container mx-auto flex flex-col p-8 justify-center items-center ">
+          <h1 className="text-3xl font-medium text-center dark:text-gray-100 pb-16">
+            Admin Page
+          </h1>
+          {authorised ? (
+            <div className="p-2 bg-white dark:bg-black transition-all ease-in-out rounded shadow-sm w-full  ">
+              <div className="flex justify-center py-5">
+                <div className="upload-card">
+                  <div className="upload-card-header">
+                    <AiOutlineCloudUpload size={100} />
+                    <p>{fileSelected ? fileName : "Browse File to upload!"}</p>
                   </div>
-                  <div className="group-hover:pb-10 transition-all duration-500 delay-200">
-                    <h1 className="font-semibold text-gray-700">Admin</h1>
-                    <p className="text-gray-500 text-sm">@developer</p>
-                  </div>
-                  <div className="flex items-center transition-all duration-500 delay-200 group-hover:bottom-3 -bottom-full absolute gap-2 justify-evenly w-full">
-                    <div className="flex gap-3 text-2xl bg-gray-700 text-white p-1 hover:p-2 transition-all duration-500 delay-200 rounded-full shadow-sm">
-                      <a className="hover:scale-110 transition-all duration-500 delay-200">
-                        <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 1024 1024">
-                          <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path>
-                        </svg>
-                      </a>
-                      <a className="hover:scale-110 transition-all duration-500 delay-200">
-                        <svg width="1em" height="1em" viewBox="0 0 24 24" stroke="currentColor" fill="none">
-                          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                          <path d="M22 6l-10 7L2 6"></path>
-                        </svg>
-                      </a>
-                      <a className="hover:scale-110 transition-all duration-500 delay-200">
-                        <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 960 1000">
-                          <path d="M480 20c133.333 0 246.667 46.667 340 140s140 206.667 140 340c0 132-46.667 245-140 339S613.333 980 480 980c-132 0-245-47-339-141S0 632 0 500c0-133.333 47-246.667 141-340S348 20 480 20M362 698V386h-96v312h96m-48-352c34.667 0 52-16 52-48s-17.333-48-52-48c-14.667 0-27 4.667-37 14s-15 20.667-15 34c0 32 17.333 48 52 48m404 352V514c0-44-10.333-77.667-31-101s-47.667-35-81-35c-44 0-76 16.667-96 50h-2l-6-42h-84c1.333 18.667 2 52 2 100v212h98V518c0-12 1.333-20 4-24 8-25.333 24.667-38 50-38 32 0 48 22.667 48 68v174h98"></path>
-                        </svg>
-                      </a>
+                  <label for="file-upload" className="upload-card-footer">
+                    {fileSelected ? (
+                      <AiOutlineCheckCircle size={55} />
+                    ) : (
+                      <AiOutlineFileAdd size={55} />
+                    )}
+                    <p className="text-white">
+                      {fileSelected ? "Click to submit" : "Not selected file"}
+                    </p>
+                  </label>
+                  <input
+                    id="file-upload"
+                    type="file"
+                    onChange={handleFileChange}
+                  />
+                </div>
+              </div>
+              <div className="text-center w-full text-xl py-5">
+                Suggestions Prompts
+              </div>
+              {suggestionList?.suggestions?.map((suggestion, index) => (
+                <div>
+                  <div
+                    key={index}
+                    className="flex  flex-row w-full justify-between mb-5"
+                  >
+                    <div className="flex flex-row space-x-4">
+                      <div className="p-2 bg-gray-200 dark:bg-gray-700 rounded">
+                        {suggestion.suggestion}
+                      </div>
+                      <div className="p-2 bg-gray-200 dark:bg-gray-700 rounded">
+                        {suggestion.answer}
+                      </div>
+                    </div>
+                    <div>
+                      <button
+                        className="bg-red-500 text-white rounded-full px-4 py-2"
+                        onClick={() => handleSuggestionRemoval(index)}
+                      >
+                        X
+                      </button>
                     </div>
                   </div>
                 </div>
+              ))}
+              <div className="flex flex-col w-full gap-y-4">
+                <input
+                  type="text"
+                  placeholder="Question"
+                  className="dark:bg-gray-800 dark:text-white border-2 border-gray-300 rounded p-2 mb-2 w-full"
+                  value={suggestion}
+                  onChange={(e) => handleSuggestionChange(e)}
+                />
+                <input
+                  type="text"
+                  placeholder="Answer"
+                  className="dark:bg-gray-800 dark:text-white border-2 border-gray-300 rounded p-2 mb-2 w-full"
+                  value={answer}
+                  onChange={(e) => handleAnswerChange(e)}
+                />
+              </div>
+              <div className="flex flex-row py-5 justify-center overflow-hidden">
+                <button
+                  className="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 "
+                  onClick={() => handleAddSuggestionClick(suggestion, answer)}
+                >
+                  <span class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                  <span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
+                    Add Suggestion
+                  </span>
+                </button>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <div className="text-red-500 text-xl text-center py-2">
+                  {" "}
+                  Danger zone
+                </div>
+                <div className="flex gap-4 border p-8 border-gray-200 md:w-1/2 rounded justify-center">
+                  <button
+                    className="px-5 py-2.5 font-medium bg-red-50 hover:bg-red-100 hover:text-red-600 text-red-500 rounded-lg text-sm"
+                    onClick={async () => {
+                      try {
+                        await axios.get(
+                          "http://34.148.188.181:4600/api/setup_model"
+                        );
+                        alert("Request to start the model has been sent!");
+                      } catch (error) {
+                        console.error("Error:", error);
+                        alert("There was an error in starting the model.");
+                      }
+                    }}
+                  >
+                    Start the Model
+                  </button>
+                  <button
+                    className="px-5 py-2.5 font-medium bg-red-50 hover:bg-red-100 hover:text-red-600 text-red-500 rounded-lg text-sm"
+                    onClick={async () => {
+                      try {
+                        await axios.get(
+                          "http://34.148.188.181:4600/api/shutdown"
+                        );
+                        alert("Request to shutdown the model has been sent!");
+                      } catch (error) {
+                        console.error("Error:", error);
+                        alert("There was an error in shutting down the model.");
+                      }
+                    }}
+                  >
+                    Shutdown the model
+                  </button>
+                </div>
               </div>
             </div>
-
-            {suggestionList?.suggestions?.map((suggestion, index) => (
-              <div key={index} className="flex  flex-row w-full justify-between mb-2">
-                <div className="flex flex-row space-x-4">
-                  <div className="p-2 bg-gray-200 dark:bg-gray-700 rounded">{suggestion.suggestion}</div>
-                  <div className="p-2 bg-gray-200 dark:bg-gray-700 rounded">{suggestion.answer}</div>
-                </div>
-                <div>
-                  <button className="dark:bg-red-500 bg-red-300 text-white rounded p-1" onClick={() => handleSuggestionRemoval(index)}>Remove</button>
-                </div>
+          ) : (
+            <div className="card ">
+              <div class="circle"></div>
+              <div class="circle"></div>
+              <div className="flex w-full justify-center items-center text-center card-inner ">
+                <form className="login-form inputGroup" onSubmit={handleSubmit}>
+                  <h1 className="text-2xl py-5">Login</h1>
+                  <input
+                    type="text"
+                    className="input-field "
+                    placeholder="Username"
+                    value={userName}
+                    onChange={handleUsernameChange}
+                  />
+                  <input
+                    type="password"
+                    className="input-field"
+                    placeholder="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                  <button type="submit" className="submit-button">
+                    Sign in
+                  </button>
+                </form>
               </div>
-            ))}
-            <div className="flex flex-col w-full">
-              <input type="text" placeholder="Question" className="dark:bg-gray-800 dark:text-white border-2 border-gray-300 rounded p-1 mb-2 w-full" value={suggestion} onChange={(e) => handleSuggestionChange(e)} />
-              <input type="text" placeholder="Answer" className="dark:bg-gray-800 dark:text-white border-2 border-gray-300 rounded p-1 mb-2 w-full" value={answer} onChange={(e) => handleAnswerChange(e)} />
             </div>
-            <div className="flex flex-row gap-5">
-              <button className="w-full dark:bg-blue-500 bg-blue-300 text-white rounded p-2 mt-2" onClick={() => handleAddSuggestionClick(suggestion, answer)}>Add Suggestion</button>
-            </div>
-          </div>
-        ) : (
-          <div className="flex w-fullnp justify-center items-center text-center">
-            <form className="login-form" onSubmit={handleSubmit}>
-              <input
-                type="text"
-                className="input-field"
-                placeholder="Username"
-                value={userName}
-                onChange={handleUsernameChange}
-              />
-              <input
-                type="password"
-                className="input-field"
-                placeholder="Password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              <button type="submit" className="submit-button">Sign in</button>
-            </form>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
