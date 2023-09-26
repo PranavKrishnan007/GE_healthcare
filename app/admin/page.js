e97430024c84008609ea5a528b59320c9bf71684
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-09-26 18:32:41 +0530

    <add> : [firebase storage for uploading pdfs]

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,8 +1,9 @@
 "use client";
 
 import { useEffect, useState } from 'react'
-import { db } from '@/app/firebase'
+import { db, storage } from '@/app/firebase'
 import { arrayUnion, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
+import { ref, uploadBytes } from "firebase/storage";
 import TopBar from '@/components/topbar/topbar'
 import axios from "axios";
 import {
@@ -30,6 +31,9 @@ const Admin = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [suggestionList, setSuggestionList] = useState({ suggestions: [] });
+  const [fileSelected, setFileSelected] = useState(false);
+  const [fileName, setFileName] = useState('');
+  const [file, setFile] = useState(null);
 
   const [suggestion, setSuggestion] = useState('');
   const [answer, setAnswer] = useState('');
@@ -46,7 +50,7 @@ const Admin = () => {
       await updateDoc(docRef, {
         "suggestions": newSuggestions
       });
-      setSuggestionList({suggestions: newSuggestions});
+      setSuggestionList({ suggestions: newSuggestions });
     } catch (error) {
       console.log("Error updating document: ", error);
     }
@@ -107,36 +111,36 @@ const Admin = () => {
     setAnswer(e.target.value);
   }
 
-   const [selectedFile, setSelectedFile] = useState(null);
-   const [fileName, setFileName] = useState("Not selected file");
-   const [fileSelected, setFileSelected] = useState(false);
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+    setFileSelected(true);
+    setFileName(event.target.files[0].name);
+  };
 
-   const handleFileChange = (event) => {
-     const file = event.target.files[0];
-     setSelectedFile(file);
-     setFileName(file.name);
-     setFileSelected(true);
-   };
+  const handleUploadSubmit = async (event) => {
+    event.preventDefault();
 
-   const handleUpload = async () => {
-     const formData = new FormData();
-     formData.append("file", selectedFile);
+    if (file) {
+      // Create a storage reference and upload the file
+      const storageRef = ref(storage, `pdfs/${file.name}`);
+      await uploadBytes(storageRef, file);
 
-     try {
-       const response = await axios.post("/api/upload", formData, {
-         headers: {
-           "Content-Type": "multipart/form-data",
-         },
-       });
-       console.log(response.data);
-     } catch (error) {
-       console.error("Error:", error);
-     }
-   };
+      // Store a reference to the uploaded file in Firestore
+      const docData = {
+        pdfUrl: `pdfs/${file.name}`,
+      };
+      await setDoc(doc(db, "pdfs", file.name), docData);
+
+      // Reset the form
+      setFileSelected(false);
+      setFileName('');
+      setFile(null);
+    }
+  };
   return (
     <div className="min-h-screen  dark:bg-[#121314]">
       <div className="px-12">
-        <TopBar admin={true}  />
+        <TopBar admin={true} />
       </div>
       <div className="bg-transparent ">
         <div className="container mx-auto flex flex-col p-8 justify-center items-center ">
@@ -146,27 +150,32 @@ const Admin = () => {
           {authorised ? (
             <div className="p-2 bg-transparent transition-all ease-in-out rounded shadow-sm w-full  ">
               <div className="flex justify-center py-5">
-                <div className="upload-card">
-                  <div className="upload-card-header">
-                    <AiOutlineCloudUpload size={100} />
-                    <p>{fileSelected ? fileName : "Browse File to upload!"}</p>
+                <form onSubmit={handleUploadSubmit}>
+                  <div className="upload-card">
+                    <div className="upload-card-header">
+                      <AiOutlineCloudUpload size={100} />
+                      <p>{fileSelected ? fileName : "Browse File to upload!"}</p>
+                    </div>
+                    <label htmlFor="file-upload" className="upload-card-footer">
+                      {fileSelected ? (
+                        <AiOutlineCheckCircle size={55} />
+                      ) : (
+                        <AiOutlineFileAdd size={55} />
+                      )}
+                      <p className="text-white">
+                        {fileSelected ? "Click to submit" : "Not selected file"}
+                      </p>
+                    </label>
+                    <input
+                      id="file-upload"
+                      type="file"
+                      onChange={handleFileChange}
+                    />
+                    <button type="submit" disabled={!fileSelected}>
+                      Upload
+                    </button>
                   </div>
-                  <label for="file-upload" className="upload-card-footer">
-                    {fileSelected ? (
-                      <AiOutlineCheckCircle size={55} />
-                    ) : (
-                      <AiOutlineFileAdd size={55} />
-                    )}
-                    <p className="text-white">
-                      {fileSelected ? "Click to submit" : "Not selected file"}
-                    </p>
-                  </label>
-                  <input
-                    id="file-upload"
-                    type="file"
-                    onChange={handleFileChange}
-                  />
-                </div>
+                </form>
               </div>
               <div className="text-center w-full text-xl py-5">
                 Suggestions Prompts
@@ -246,18 +255,18 @@ const Admin = () => {
                     Start the Model
                   </button>
                   <button
-                      className="px-5 py-2.5 font-medium bg-red-50 hover:bg-red-100 hover:text-red-600 text-red-500 rounded-lg text-sm"
-                      onClick={async () => {
-                        try {
-                          await axios.get(
-                              "http://34.148.188.181:4600/api/check_health"
-                          );
-                          alert("Request to check health the model has been sent!");
-                        } catch (error) {
-                          console.error("Error:", error);
-                          alert("There was an error in health the model.");
-                        }
-                      }}
+                    className="px-5 py-2.5 font-medium bg-red-50 hover:bg-red-100 hover:text-red-600 text-red-500 rounded-lg text-sm"
+                    onClick={async () => {
+                      try {
+                        await axios.get(
+                          "http://34.148.188.181:4600/api/check_health"
+                        );
+                        alert("Request to check health the model has been sent!");
+                      } catch (error) {
+                        console.error("Error:", error);
+                        alert("There was an error in health the model.");
+                      }
+                    }}
                   >
                     check health
                   </button>
