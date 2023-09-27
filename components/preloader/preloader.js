3bb7131f412be61f6b912961c2cf19fab5b7fe7f
Sahithya Kattamuri <aythihas2003@gmail.com>
2023-09-27 16:52:29 +0530

    Admin fix (#10)
    * changes in admin
    
    * changes in admin page
    
    ---------
    
    Co-authored-by: rithulr808 <rithulravichnadra@gmail.com>

--- components/preloader/preloader.js
+++ components/preloader/preloader.js
@@ -7,14 +7,14 @@ const PreLoader = () => {
     }, []);
     return (
         <div className="preloader dark:bg-[#121314] shadow-2xl">
-            <span className="absolute top-12 left-12 text-2xl dark:text-white  font-bold text-black px-3">
-                BLEH
-            </span>
+            {/*<span className="absolute top-12 left-12 text-2xl dark:text-white  font-bold text-black px-3">*/}
+            {/*    */}
+            {/*</span>*/}
             <div className="texts-container dark:text-white text-4xl">
-                <span className="px-2">Believe,</span>
-                <span className="px-2">Learn,</span>
-                <span className="px-2">Explore,</span>
-                <span className="px-2">Help.</span>
+                <span className="px-2"><span className="text-[#4a25e1]">b</span>elieve.</span>
+                <span className="px-2"><span className="text-[#4a25e1]">l</span>earn.</span>
+                <span className="px-2"><span className="text-[#4a25e1]">e</span>xplore.</span>
+                <span className="px-2"><span className="text-[#4a25e1]">h</span>elp.</span>
             </div>
         </div>
     );
